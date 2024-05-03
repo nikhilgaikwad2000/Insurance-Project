90725ac7e12b1cfedaa51694865c04a1ebfba35d
Nikhil Gaikwad <122869789+nikhilgaikwad2000@users.noreply.github.com>
2024-05-03 20:05:04 +0530

    Add files via upload

---
+++ JRTP_31_BI_API/src/main/java/in/ashokit/Jrtp31BiApiApplication.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jrtp31BiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jrtp31BiApiApplication.class, args);
+	}
+
+}
